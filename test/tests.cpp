036a0abedf615e6f3e4eed7ce6e0440ac23dc5a1
Bogdan__ <bogerbloger@yandex.ru>
2025-02-24 20:24:03 +0300

    ggg

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,62 +4,50 @@
 #include <cstdint>
 #include "alg.h"
 
-TEST(st1, checkPrime2)
-{
+TEST(st1, checkPrime2){
   EXPECT_EQ(checkPrime(4), false);
 }
 
-TEST(st1, checkPrime3)
-{
+TEST(st1, checkPrime3){
   EXPECT_EQ(checkPrime(17), true);
 }
 
-TEST(st1, checkPrime4)
-{
+TEST(st1, checkPrime4){
   EXPECT_EQ(checkPrime(9973), true);
 }
 
-TEST(st1, checkPrime5)
-{
+TEST(st1, checkPrime5){
   EXPECT_EQ(checkPrime(20000), false);
 }
 
-TEST(st1, nPrime1)
-{
+TEST(st1, nPrime1){
   EXPECT_EQ(nPrime(6), 13);
 }
 
-TEST(st1, nPrime2)
-{
+TEST(st1, nPrime2){
   EXPECT_EQ(nPrime(10), 29);
 }
 
-TEST(st1, nPrime3)
-{
+TEST(st1, nPrime3){
   EXPECT_EQ(nPrime(15), 47);
 }
 
-TEST(st1, nextPrime1)
-{
+TEST(st1, nextPrime1){
   EXPECT_EQ(nextPrime(13), 17);
 }
 
-TEST(st1, nextPrime2)
-{
+TEST(st1, nextPrime2){
   EXPECT_EQ(nextPrime(19), 23);
 }
 
-TEST(st1, nextPrime3)
-{
+TEST(st1, nextPrime3){
   EXPECT_EQ(nextPrime(1000), 1009);
 }
 
-TEST(st1, nextPrime4)
-{
+TEST(st1, nextPrime4){
   EXPECT_EQ(nextPrime(7919), 7927);
 }
 
-TEST(st1, edgeCases)
-{
+TEST(st1, edgeCases){
   EXPECT_EQ(nPrime(100), 541);
 }
